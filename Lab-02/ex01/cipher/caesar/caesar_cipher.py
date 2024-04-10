c3dce4540a0488673ca5d41ccbb75989bd143f11
VuTienChien <vvchien122@gmail.com>
2024-04-10 09:00:29 +0700

    [add] lab-02 ex01 caesar

---
+++ Lab-02/ex01/cipher/caesar/caesar_cipher.py
@@ -0,0 +1,27 @@
+from cipher.caesar import ALPHABET
+
+class Caesar_Cipher:
+    def __init__(self):
+        self.alphabet = ALPHABET
+
+    def encrypt_text(self,text:str,key:int) ->str:
+        alphabet_len = len(self.alphabet)
+        text = text.upper()
+        encrypt_text=[]
+        for letter in text:
+            leeter_index = self.alphabet.index(letter)
+            output_index = (leeter_index + key) % alphabet_len
+            output_letter = self.alphabet[output_index]
+            encrypt_text.append(output_letter)
+        return "".join(encrypt_text)
+    
+    def decrypt_text(self,text:str,key:int) ->str:
+        alphabet_len = len(self.alphabet)
+        text = text.upper()
+        decrypt_text=[]
+        for letter in text:
+            leeter_index = self.alphabet.index(letter)
+            output_index = (leeter_index + key) % alphabet_len
+            output_letter = self.alphabet[output_index]
+            decrypt_text.append(output_letter)
+        return "".join(decrypt_text)
