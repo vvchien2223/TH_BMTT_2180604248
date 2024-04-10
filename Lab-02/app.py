34412162066cde16895814374c1ffdc0f220494f
VuChien <vvchien122@gmail.com>
2024-04-10 16:39:25 +0700

    [Add] Done Flask

--- Lab-02/app.py
+++ Lab-02/app.py
@@ -1,5 +1,9 @@
 from flask import Flask,render_template,request,json
-from  cipher.caesar import Caesar_Cipher
+from cipher.caesar import Caesar_Cipher
+from cipher.vigenere import VigenereCipher
+from cipher.playfair import PlayFairCipher
+from cipher.railfence import RailFenceCipher
+from cipher.transpostion import TranspositionCipher
 
 app = Flask(__name__)
 
@@ -13,9 +17,25 @@ def home():
 def caesar():
     return render_template('caesar.html')
 
-@app.route("/encrypt",methods=['POST'])
+@app.route("/vigenere")
+def vigenere():
+    return render_template('vigenere.html')
+
+@app.route("/railfence")
+def railfence():
+    return render_template('railfence.html')
+
+@app.route("/transpostion")
+def transpostion():
+    return render_template('/transpostion.html')
+
+@app.route("/playfair")
+def playfair():
+    return render_template('/playfair.html')
+
+#CAESAR 
+@app.route("/caesar/encrypt",methods=['POST'])
 def caesar_encrypt():
-    
     text = request.form['inputPlainText']
     key = int(request.form['inputKeyPlain'])
     caesar_cipher = Caesar_Cipher()
@@ -26,11 +46,10 @@ def caesar_encrypt():
         <p><strong>Key:</strong> {key}</p>
         <p><strong>Encrypted Text:</strong> {encrypted_text}</p>
             """
-
     return response
 
 
-@app.route("/decrypt",methods=['POST'])
+@app.route("/caesar/decrypt",methods=['POST'])
 def caesar_decrypt():
     text = request.form['inputCipherText']
     key = int(request.form['inputKeyCipher'])
@@ -42,8 +61,146 @@ def caesar_decrypt():
         <p><strong>Key:</strong> {key}</p>
         <p><strong>Decrypted Text:</strong> {decrypted_text}</p>
             """
-
     return response  
+
+
+#VIGENERE CIPHER ALGORITHM
+@app.route('/vigenere/encrypt',methods=['POST'])
+def vigenere_encrypt():
+    text = request.form['inputPlainText']
+    key = request.form['inputKeyPlain']
+    vigenere_cipher = VigenereCipher()
+    encrypted_text = vigenere_cipher.vigenere_encrypt(text,key)
+    response = f"""
+        <h1>ĐÃ MÃ HÓA THÀNH CÔNG!</h1>
+        <p><strong>Text:</strong> {text}</p>
+        <p><strong>Key:</strong> {key}</p>
+        <p><strong>Decrypted Text:</strong> {encrypted_text}</p>
+            """
+    return response 
+
+@app.route('/vigenere/decrypt',methods=['POST'])
+def vigenere_decrypt():
+    text = request.form['inputCipherText']
+    key = request.form['inputKeyCipher']
+    vigenere_cipher = VigenereCipher()
+    decrypted_text = vigenere_cipher.vigenere_decrypt(text,key)
+    response = f"""
+        <h1>ĐÃ GIẢI MÃ THÀNH CÔNG!</h1>
+        <p><strong>Text:</strong> {text}</p>
+        <p><strong>Key:</strong> {key}</p>
+        <p><strong>Decrypted Text:</strong> {decrypted_text}</p>
+            """
+    return response 
+
+#RAILFENCE
+
+@app.route("/railfence/encrypt",methods=['POST'])
+def railfence_encrypt():
+    text = request.form['inputPlainText']
+    key = int(request.form['inputKeyPlain'])
+    railfence_cipher = RailFenceCipher()
+    encrypted_text = railfence_cipher.rail_fence_encrypt(text, key)
+    response = f"""
+        <h1>Đã Mã Hóa Thành Công!</h1>
+        <p><strong>Text:</strong> {text}</p>
+        <p><strong>Key:</strong> {key}</p>
+        <p><strong>Encrypted Text:</strong> {encrypted_text}</p>
+            """
+    return response
+
+
+@app.route("/railfence/decrypt",methods=['POST'])
+def railfence_decrypt():
+    text = request.form['inputCipherText']
+    key = int(request.form['inputKeyCipher'])
+    railfence_cipher = RailFenceCipher()
+    decrypted_text = railfence_cipher.rail_fence_decrypt(text, key)
+    response = f"""
+        <h1>ĐÃ GIẢI MÃ THÀNH CÔNG!</h1>
+        <p><strong>Text:</strong> {text}</p>
+        <p><strong>Key:</strong> {key}</p>
+        <p><strong>Decrypted Text:</strong> {decrypted_text}</p>
+            """
+    return response  
+
+
+#Transpostion
+
+@app.route("/transpostion/encrypt",methods=['POST'])
+def transpostion_encrypt():
+    text = request.form['inputPlainText']
+    key = int(request.form['inputKeyPlain'])
+    transpostion_cipher = TranspositionCipher()
+    encrypted_text = transpostion_cipher.encrypt(text, key)
+    response = f"""
+        <h1>Đã Mã Hóa Thành Công!</h1>
+        <p><strong>Text:</strong> {text}</p>
+        <p><strong>Key:</strong> {key}</p>
+        <p><strong>Encrypted Text:</strong> {encrypted_text}</p>
+            """
+    return response
+
+
+@app.route("/transpostion/decrypt",methods=['POST'])
+def transpostion_decrypt():
+    text = request.form['inputCipherText']
+    key = int(request.form['inputKeyCipher'])
+    transpostion_cipher = TranspositionCipher()
+    decrypted_text = transpostion_cipher.decrypt(text, key)
+    response = f"""
+        <h1>ĐÃ GIẢI MÃ THÀNH CÔNG!</h1>
+        <p><strong>Text:</strong> {text}</p>
+        <p><strong>Key:</strong> {key}</p>
+        <p><strong>Decrypted Text:</strong> {decrypted_text}</p>
+            """
+    return response  
+
+
+
+#Playfair
+
+@app.route('/playfair/creatematrix',methods=['POST'])
+def playfair_creatematrix():
+    key = request.form['inputKeyMatrix']
+    playfair_cipher = PlayFairCipher()
+    playfair_matrix = playfair_cipher.create_playfair_matrix(key)
+    response = f"""
+        <h1>ĐÃ TẠO THÀNH CÔNG MA TRẬN!</h1>
+        <p><strong>Key:</strong> {key}</p>
+        <p><strong>Matrix :</strong> {playfair_matrix}</p>
+            """
+    return response 
+
+@app.route('/playfair/encrypt',methods=['POST'])
+def playfair_encrypt():
+    text = request.form['inputPlainText']
+    key = request.form['inputKeyPlain']
+    playfair_cipher = PlayFairCipher()
+    playfair_matrix = playfair_cipher.create_playfair_matrix(key)
+    encrypted_text = playfair_cipher.playfair_encrypt(text,playfair_matrix)
+    response = f"""
+        <h1>ĐÃ MÃ HÓA THÀNH CÔNG!</h1>
+        <p><strong>Text:</strong> {text}</p>
+        <p><strong>Key:</strong> {key}</p>
+        <p><strong>Decrypted Text:</strong> {encrypted_text}</p>
+            """
+    return response 
+
+@app.route('/playfair/decrypt',methods=['POST'])
+def playfair_decrypt():
+    text = request.form['inputCipherText']
+    key = request.form['inputKeyCipher']
+    playfair_cipher = PlayFairCipher()
+    playfair_matrix = playfair_cipher.create_playfair_matrix(key)
+    decrypted_text = playfair_cipher.playfair_decrypt(text,playfair_matrix)
+    response = f"""
+        <h1>ĐÃ GIẢI MÃ THÀNH CÔNG!</h1>
+        <p><strong>Text:</strong> {text}</p>
+        <p><strong>Key:</strong> {key}</p>
+        <p><strong>Decrypted Text:</strong> {decrypted_text}</p>
+            """
+    return response 
 
 
 #main function
